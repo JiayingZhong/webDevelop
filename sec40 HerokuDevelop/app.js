6e84bc01c06f8e99e031b786fadfa5c6a28119a2
zjy <zhongjia@usc.edu>
2020-08-01 12:55:08 -0700

    add sec40

--- sec40 HerokuDevelop/app.js
+++ sec40 HerokuDevelop/app.js
@@ -12,6 +12,10 @@ app.get("/about", function(req, res){
     res.render("about");
 });
 
-app.listen(3000, function(){
+// app.listen(3000, function(){
+//     console.log("running");
+// });
+
+app.listen(process.env.PORT, process.env.IP, function(){
     console.log("running");
 });
